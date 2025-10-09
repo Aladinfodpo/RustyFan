bba7ad5a5941338b85f8f0895baadcd480ec37bf
aladinfodop <nicolas.beauquin@hotmail.fr>
2025-10-09 23:57:21 +0200

    Wip geogebra

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::ops;
+mod equation;
+use core::f32;
+use std::{ops};
 
 struct Helice{
     curve : Curve
@@ -11,7 +13,7 @@ struct Point{
 }
 
 struct Curve {
-    points : [Point; Curve::NUM_POINTS]
+    points : Vec<Point>
 }
 
 impl ops::Mul<Point> for f32{
@@ -37,39 +39,46 @@ impl Point{
 }
 
 impl Curve {
-    pub const NUM_POINTS : usize = 12;
+    fn sample_from_function(f : equation::GenerativeFunction, n : i32, start : f32, end : f32) -> Curve{
+        Curve{points : (0..n).map( |j| {Point{fields :
+                core::array::from_fn(|i| {
+                    let x = start + (end - start) * (j as f32) / ((n-1) as f32);
+                    if i == 0 {x} else {f(x)}
+                })}
+            }).collect()}
+    }
 
-    fn create() -> Curve{
-        Curve{points : core::array::from_fn( |j| {Point{fields :
+    fn create(n : i32) -> Curve{
+        Curve{points : (0..n).map( |j| {Point{fields :
                 core::array::from_fn(|i| {
                     if i == 0 {j as f32} else {j as f32 * 100f32}
                 })}
-            })}
+            }).collect()}
     }
 
     fn print(&self){
-        for p in self.points{
+        for p in &self.points{
             println!("Point debit = {}, pression = {}", p.fields[0], p.fields[1]);
         }
     }
 
     fn interpolated(&self, t : f32) -> Point{
         match self.points.iter().position(|&p| p.fields[0] > t){
-            None => self.points[Curve::NUM_POINTS-1],
+            None => *self.points.last().expect("Empty curve"),
             Some(next) if next > 0 => {
                 let prec = next - 1;
 
                 Point::interpolate(&self.points[prec], &self.points[next], (t - self.points[prec].fields[0]) / (self.points[next].fields[0] - self.points[prec].fields[0]) )
             },
-            Some(_) => self.points[0]
+            Some(_) => *self.points.first().expect("Empty curve")
         }
-        
     }
 }
 
 
 fn main() {
-    let c = Curve::create();
+    //let c = Curve::create(10);
+    let c = Curve::sample_from_function(f32::sin, 10, 0f32, f32::consts::PI);
     c.print();
-    println!("Interpolated = {}", c.interpolated(3.5).fields[1]);
+    println!("Interpolated = {}", c.interpolated(10.5).fields[1]);
 }
